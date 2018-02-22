d19c3732f454f7f356de1dd4332dc233e641e980
Amanda Hittelman <ahittelm@ucsd.edu>
2018-02-22 04:45:07 -0800

    starting to integrate timer

--- routes/addworkout.js
+++ routes/addworkout.js
@@ -2,7 +2,29 @@
  * GET home page.
  */
 var data = require('../exercises.json');
+var Handlebars = require('handlebars/runtime');
+
 
 exports.view = function(req, res){
   res.render('addworkout', data);
 };
+
+
+Handlebars.registerHelper('ifIsZero', function(value, options) {
+  if(value === 0) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+// function init(){
+// 	document.getElementById("addbtn").onclick = function(){
+// 			location.href = "addworkout"
+// 			var length = Object.keys(data.workout.exercises).length;
+// 			var exname = "Exercise Name";
+// 			var time = "Time"
+// 			var unit = "Unit"
+// 			var sound = "Sound"
+// 			data.workout.exercises[length] = {exname, time, unit, sound};
+// 	};
+// } 
