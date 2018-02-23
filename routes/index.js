ebbec8f2ea20986a21295eee680571fdbaf73b83
Amanda Hittelman <ahittelm@ucsd.edu>
2018-02-22 16:40:15 -0800

    trying to work out the login page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 /*
  * GET home page.
  */
+var login = require('../login.json');
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', login);
 };
+
+
+function submitClicked() {
+	// your code here
+	$("#submit").click(function () {
+		console.log(login.account);
+	});
+}
