cc7bec9476633b4a53f5eaa848387efc98b72ac4
Amanda Hittelman <ahittelm@ucsd.edu>
2018-02-28 19:52:57 -0800

    hi

--- routes/editworkout.js
+++ routes/editworkout.js
@@ -5,7 +5,8 @@ var newExercise = require('../newExercise.json');
 
 exports.view = function(req, res){
   var workout = data.workout[req.params.id];
-  res.render('editworkout', {workout: workout});
+  var sounds = data.sounds;
+  res.render('editworkout', {workout: workout, id:req.params.id, sounds:sounds});
 };
 
 function addnewexercise(){
